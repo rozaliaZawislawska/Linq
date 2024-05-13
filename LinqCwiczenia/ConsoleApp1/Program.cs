62083ae08ddad24229ce65c50b0a9bf6c02bff81
rozaliaZawislawska <s28052@pjwstk.edu.pl>
2024-05-13 09:59:05 +0200

    Linq

---
+++ LinqCwiczenia/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task4();
+            foreach (var emp in t)
+            {
+                Console.WriteLine(emp);
+            }
+        }
+    }
+}
